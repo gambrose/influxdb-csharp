218b3b79d62e46c30cc73665b1ecd3fb670e978d
Graham Amborse <graham.ambrose@enmodus.com>
2017-08-20 11:41:29 +0100

    Added benchmark for writing out line protocol points.

---
+++ sample/Benchmark/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Benchmark
+{
+    using System;
+    using BenchmarkDotNet.Running;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<WriteLineProtocol>();
+
+            Console.ReadKey();
+        }
+    }
+}
